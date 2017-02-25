b021e0eb8c13dd2b1b295e162e035f347ad57038
Senh Linsh <lyuanxiang0812@126.com>
2017-02-25 20:49:47 +0800

    细节修改

--- lshutils/src/main/java/com/linsh/lshutils/utils/LshDeviceUtils.java
+++ lshutils/src/main/java/com/linsh/lshutils/utils/LshDeviceUtils.java
@@ -1,7 +1,8 @@
 package com.linsh.lshutils.utils;
 
-import android.content.Context;
 import android.content.res.Configuration;
+
+import com.linsh.lshutils.utils.Basic.LshApplicationUtils;
 
 /**
  * Created by Senh Linsh on 16/10/21.
@@ -10,11 +11,11 @@ public class LshDeviceUtils {
 
     /**
      * 判断是否平板设备
-     * @param context
+     *
      * @return true:平板,false:手机
      */
-    public static boolean isPhoneDevice(Context context) {
-        return (context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) <
-                Configuration.SCREENLAYOUT_SIZE_LARGE;
+    public static boolean isPhoneDevice() {
+        return (LshApplicationUtils.getContext().getResources().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK) < Configuration.SCREENLAYOUT_SIZE_LARGE;
     }
 }
