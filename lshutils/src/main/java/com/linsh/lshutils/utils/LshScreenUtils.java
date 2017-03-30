132af876275e39c91136a0fb326fc3f11f987a59
Senh Linsh <lyuanxiang0812@126.com>
2017-03-30 18:32:18 +0800

    Add LshScreenUtils.

---
+++ lshutils/src/main/java/com/linsh/lshutils/utils/LshScreenUtils.java
@@ -0,0 +1,50 @@
+package com.linsh.lshutils.utils;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.Surface;
+import android.view.WindowManager;
+
+import com.linsh.lshutils.utils.Basic.LshApplicationUtils;
+
+import static com.linsh.lshutils.utils.LshScreenUtils.Orientation.LANDSCAPE;
+import static com.linsh.lshutils.utils.LshScreenUtils.Orientation.PORTRAIT;
+
+/**
+ * Created by Senh Linsh on 17/3/30.
+ */
+
+public class LshScreenUtils {
+
+    public static int getScreenWidth() {
+        WindowManager wm = (WindowManager) LshApplicationUtils.getContext().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.widthPixels;
+    }
+
+    public static int getScreenHeight() {
+        WindowManager wm = (WindowManager) LshApplicationUtils.getContext().getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.heightPixels;
+    }
+
+    public static int getScreenRotation() {
+        WindowManager wm = (WindowManager) LshApplicationUtils.getContext().getSystemService(Context.WINDOW_SERVICE);
+        return wm.getDefaultDisplay().getRotation();
+    }
+
+    public static Orientation getScreenOritation() {
+        int rotation = getScreenRotation();
+        if (rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_180) {
+            return PORTRAIT;
+        } else {
+            return LANDSCAPE;
+        }
+    }
+
+    public enum Orientation {
+        LANDSCAPE, PORTRAIT
+    }
+}
