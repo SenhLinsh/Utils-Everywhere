6a26d414d4c55f6f17aae54943cc881a5698498b
Senh Linsh <lyuanxiang0812@126.com>
2017-04-25 20:33:50 +0800

    Add LshThreadUtils

---
+++ lshutils/src/main/java/com/linsh/lshutils/utils/LshThreadUtils.java
@@ -0,0 +1,12 @@
+package com.linsh.lshutils.utils;
+
+/**
+ * Created by Senh Linsh on 17/4/25.
+ */
+
+public class LshThreadUtils {
+
+    public static String getCurrentThreadName() {
+        return Thread.currentThread().getName();
+    }
+}
