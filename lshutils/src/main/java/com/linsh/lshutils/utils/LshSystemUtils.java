169038ff39a58634653274a1760aec34323de92f
Senh Linsh <lyuanxiang0812@126.com>
2017-04-26 14:05:07 +0800

    update LshSystemUtils.

--- lshutils/src/main/java/com/linsh/lshutils/utils/LshSystemUtils.java
+++ lshutils/src/main/java/com/linsh/lshutils/utils/LshSystemUtils.java
@@ -1,6 +1,7 @@
 package com.linsh.lshutils.utils;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Build;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,20 @@ import com.linsh.lshutils.utils.Basic.LshLogUtils;
  * Created by Senh Linsh on 16/1/11.
  */
 public class LshSystemUtils {
+
+    public static void setTransparentStatusBar(Activity activity, int cantHandleColor) {
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = activity.getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            activity.getWindow().setNavigationBarColor(Color.TRANSPARENT);
+            activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+        } else {
+            setStatusBarColor(activity, cantHandleColor);
+        }
+    }
 
     /**
      * 隐藏状态栏和虚拟按键
