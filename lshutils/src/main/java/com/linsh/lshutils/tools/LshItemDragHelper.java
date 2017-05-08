587cd56a12e114069e4aa0ce6ea0451d0dd0c4e8
Senh Linsh <lyuanxiang0812@126.com>
2017-05-08 20:13:04 +0800

    添加 LshItemDragHelper，辅助实现RecyclerView条目可拖拽排序、滑动删除的效果

---
+++ lshutils/src/main/java/com/linsh/lshutils/tools/LshItemDragHelper.java
@@ -0,0 +1,124 @@
+package com.linsh.lshutils.tools;
+
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+/**
+ * Created by Senh Linsh on 17/5/8.
+ */
+
+public class LshItemDragHelper extends ItemTouchHelper {
+
+    // 使用接口模式, 可以通过 Adapter 实现 IItemDragCallback 接口来让其在Adapter内部进行处理
+    public LshItemDragHelper(IItemDragCallback callback) {
+        super(new CallbackImpl(callback));
+    }
+
+    // 使用继承模式, 可以在Adapter里面创建内部类来实现, 方便获取更多的方法来实现更多的功能
+    public LshItemDragHelper(ItemDragCallback callback) {
+        super(callback);
+    }
+
+    public abstract static class ItemDragCallback extends Callback {
+
+        @Override
+        public abstract boolean isItemViewSwipeEnabled();
+
+        @Override
+        public abstract boolean isLongPressDragEnabled();
+
+        /**
+         * 当用户拖拽或者滑动Item时, 告诉系统ItemTouchHelper滑动或者拖拽的方向
+         */
+        @Override
+        public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+            int dragFlag;
+            int swipeFlag = ItemTouchHelper.START | ItemTouchHelper.END;
+            RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+            if (layoutManager instanceof GridLayoutManager) {
+                // 网格布局只有拖拽没有滑动删除
+                dragFlag = ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT | ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+                swipeFlag = 0;
+            } else if (layoutManager instanceof LinearLayoutManager) {
+                LinearLayoutManager linearLayoutManager = (LinearLayoutManager) layoutManager;
+                int orientation = linearLayoutManager.getOrientation();
+                // 根据RecyclerView方向判断拖拽方向
+                if (orientation == LinearLayoutManager.VERTICAL) {
+                    dragFlag = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+                } else {
+                    dragFlag = ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT;
+                }
+            } else {
+                // 不知道情况的, 上下左右都能拖动吧
+                dragFlag = ItemTouchHelper.LEFT | ItemTouchHelper.DOWN | ItemTouchHelper.UP | ItemTouchHelper.RIGHT;
+            }
+            return makeMovementFlags(dragFlag, swipeFlag);
+        }
+
+        /**
+         * 每次进行拖拽排序时, ItemTouchHelper都会一直在进行相邻两个item的替换操作, 所以方法里面不宜进行耗时操作
+         */
+        @Override
+        public abstract boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target);
+
+        @Override
+        public abstract void onSwiped(RecyclerView.ViewHolder viewHolder, int direction);
+    }
+
+    public interface IItemDragCallback {
+
+        boolean isItemViewSwipeEnabled();
+
+        boolean isLongPressDragEnabled();
+
+        boolean onMove(RecyclerView recyclerView, int fromPosition, int toPosition);
+
+        void onMoved(RecyclerView recyclerView, int fromPosition, int toPosition);
+
+        void onSwiped(int position, int direction);
+    }
+
+    private static class CallbackImpl extends ItemDragCallback {
+
+        private IItemDragCallback mICallback;
+
+        public CallbackImpl(IItemDragCallback callback) {
+            if (callback == null) {
+                throw new RuntimeException("IItemDragCallback must not be null");
+            }
+            mICallback = callback;
+        }
+
+        @Override
+        public boolean isItemViewSwipeEnabled() {
+            return mICallback.isItemViewSwipeEnabled();
+        }
+
+        @Override
+        public boolean isLongPressDragEnabled() {
+            return mICallback.isLongPressDragEnabled();
+        }
+
+        @Override
+        public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+            return super.getMovementFlags(recyclerView, viewHolder);
+        }
+
+        @Override
+        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+            return mICallback.onMove(recyclerView, viewHolder.getAdapterPosition(), target.getAdapterPosition());
+        }
+
+        @Override
+        public void onMoved(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, int fromPos, RecyclerView.ViewHolder target, int toPos, int x, int y) {
+            mICallback.onMoved(recyclerView, fromPos, toPos);
+        }
+
+        @Override
+        public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+            mICallback.onSwiped(viewHolder.getAdapterPosition(), direction);
+        }
+    }
+}
