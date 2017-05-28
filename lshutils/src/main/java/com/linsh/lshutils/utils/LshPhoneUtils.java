a31cdd30ac62f887f2eec0401dc3962be2ed8b75
SenhLinsh <lyuanxiang0812@126.com>
2017-05-28 22:00:37 +0800

    更新 LshPhoneUtils 新增过去手机硬件信息的方法

--- lshutils/src/main/java/com/linsh/lshutils/utils/LshPhoneUtils.java
+++ lshutils/src/main/java/com/linsh/lshutils/utils/LshPhoneUtils.java
@@ -7,6 +7,10 @@ import android.os.Build;
 
 import com.linsh.lshutils.utils.Basic.LshApplicationUtils;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Senh Linsh on 17/2/21.
  */
@@ -61,5 +65,26 @@ public class LshPhoneUtils {
      */
     public static String getSDKVersionName() {
         return Build.VERSION.RELEASE;
+    }
+
+    /**
+     * 获取手机的硬件信息
+     */
+    public static List<String> getMobileInfo() {
+        ArrayList<String> list = new ArrayList<>();
+        // 通过反射获取系统的硬件信息
+        try {
+            Field[] fields = Build.class.getDeclaredFields();
+            for (Field field : fields) {
+                //暴力反射 ,获取私有的信息
+                field.setAccessible(true);
+                String name = field.getName();
+                String value = field.get(null).toString();
+                list.add(name + "=" + value);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 }
