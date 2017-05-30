9355a64ec2011ee903c0ee14010cec8c54c201c4
SenhLinsh <lyuanxiang0812@126.com>
2017-05-30 23:33:01 +0800

    更新 LshApplicationUtils 移除获取真正包名的方法

--- lshutils/src/main/java/com/linsh/lshutils/utils/Basic/LshApplicationUtils.java
+++ lshutils/src/main/java/com/linsh/lshutils/utils/Basic/LshApplicationUtils.java
@@ -6,9 +6,6 @@ import android.content.Context;
 import android.os.Handler;
 import android.os.Process;
 
-import com.linsh.lshutils.utils.LshArrayUtils;
-import com.linsh.lshutils.utils.LshClassUtils;
-
 /**
  * Created by Senh Linsh on 16/12/23.
  */
@@ -18,8 +15,6 @@ public class LshApplicationUtils {
     private static int mainTid;
     private static Handler mainHandler;
 
-    private static String realPackageName;
-
     public static void init(Application application) {
         // 初始化context
         appContext = application.getApplicationContext();
@@ -27,8 +22,6 @@ public class LshApplicationUtils {
         mainTid = Process.myTid();
         // 初始化handler
         mainHandler = new Handler();
-        // 设置真正的包名
-        setRealPackageName(application);
     }
 
     public static void init(Activity activity) {
@@ -66,40 +59,5 @@ public class LshApplicationUtils {
 
     public static String getPackageName() {
         return appContext.getPackageName();
-    }
-
-    /**
-     * 获取应用真正的包名, 即清单文件里声明的包名, 用来存放R文件和BuildConfig, 以及常用来作为项目的一级包名
-     */
-    private static void setRealPackageName(Application application) {
-        String contextPac = application.getApplicationContext().getPackageName();
-        String applicationPac = application.getClass().getPackage().getName();
-        if (applicationPac.contains(contextPac)) {
-            // 可以匹配上, 包名没有添加后缀
-            realPackageName = contextPac;
-        } else {
-            // gradle中修改ApplicationId 或者 添加包名后缀
-            String[] applicationSplit = applicationPac.split("\\.");
-            for (int i = applicationSplit.length - 1; i > 0; i--) {
-                String checkPac = LshArrayUtils.joint(applicationSplit, i, ".");
-                if (LshClassUtils.isClassExist(checkPac + ".BuildConfig")) {
-                    realPackageName = checkPac;
-                    return;
-                }
-            }
-            realPackageName = contextPac;
-        }
-        // FIXME: 17/3/14
-        // 当前方法有可能无法获取真正的包名
-    }
-
-    /**
-     * 获取应用真正的包名
-     * 1. gradle可以重新指定包名
-     * 2. gradle可以给PackageName添加后缀
-     * 从而导致Context.getPackageName()无法获取真正的包名
-     */
-    public static String getRealPackageName() {
-        return realPackageName;
     }
 }
