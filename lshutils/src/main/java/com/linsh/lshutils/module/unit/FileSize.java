9c13871337cd7c9141c4cdf19a5fe5260abc9ec1
SenhLinsh <lyuanxiang0812@126.com>
2017-06-06 18:26:48 +0800

    更新 FileSize 添加文件大小转换方法

--- lshutils/src/main/java/com/linsh/lshutils/module/unit/FileSize.java
+++ lshutils/src/main/java/com/linsh/lshutils/module/unit/FileSize.java
@@ -5,8 +5,17 @@ package com.linsh.lshutils.module.unit;
  */
 
 public class FileSize {
+
     public static final int B = 0;
     public static final int KB = 1;
     public static final int MB = 2;
     public static final int GB = 3;
+
+    public static float format(float size, @Unit.FileSizeDef int srcUnit, @Unit.FileSizeDef int destUnit) {
+        return (float) (size * Math.pow(1024L, (srcUnit - destUnit)));
+    }
+
+    public static float formatByte(long size, @Unit.FileSizeDef int unit) {
+        return format(size, B, unit);
+    }
 }
