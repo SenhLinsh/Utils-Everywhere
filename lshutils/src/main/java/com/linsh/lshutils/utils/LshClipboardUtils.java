54cff60db32006589e22e59f5412ed6fe5d06be4
SenhLinsh <lyuanxiang0812@126.com>
2017-06-20 18:23:48 +0800

    新增 LshClipboardUtils 复制文本到剪贴板以及获取剪贴板文本

---
+++ lshutils/src/main/java/com/linsh/lshutils/utils/LshClipboardUtils.java
@@ -0,0 +1,38 @@
+package com.linsh.lshutils.utils;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+
+import com.linsh.lshutils.utils.Basic.LshApplicationUtils;
+
+/**
+ * Created by Senh Linsh on 17/6/20.
+ */
+
+public class LshClipboardUtils {
+
+    /**
+     * 复制文本
+     */
+    public static void putText(String text) {
+        // 得到剪贴板管理器
+        ClipboardManager manager = (ClipboardManager) LshApplicationUtils.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+        manager.setPrimaryClip(ClipData.newPlainText(null, text.trim()));
+    }
+
+    /**
+     * 粘贴文本
+     *
+     * @return 当剪贴板没有东西时或者是其他类型的数据时返回 false
+     */
+    public static String getText() {
+        ClipboardManager manager = (ClipboardManager) LshApplicationUtils.getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = manager.getPrimaryClip();
+        if (clip != null && clip.getItemCount() > 0) {
+            CharSequence text = clip.getItemAt(0).getText();
+            return text == null ? null : text.toString();
+        }
+        return null;
+    }
+}
