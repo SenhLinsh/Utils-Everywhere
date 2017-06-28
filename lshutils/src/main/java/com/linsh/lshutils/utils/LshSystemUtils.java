0c3309d56e4f08f7d9f0fcf65745af8fa7ea53fb
SenhLinsh <lyuanxiang0812@126.com>
2017-06-28 16:27:30 +0800

    更新 LshSystemUtils 对设置半透明状态栏的方法进行优化调整

--- lshutils/src/main/java/com/linsh/lshutils/utils/LshSystemUtils.java
+++ lshutils/src/main/java/com/linsh/lshutils/utils/LshSystemUtils.java
@@ -6,7 +6,7 @@ import android.os.Build;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.LinearLayout;
+import android.widget.FrameLayout;
 
 import com.linsh.lshutils.utils.Basic.LshLogUtils;
 
@@ -14,20 +14,6 @@ import com.linsh.lshutils.utils.Basic.LshLogUtils;
  * Created by Senh Linsh on 16/1/11.
  */
 public class LshSystemUtils {
-
-    public static void setTransparentStatusBar(Activity activity, int cantHandleColor) {
-        if (Build.VERSION.SDK_INT >= 21) {
-            View decorView = activity.getWindow().getDecorView();
-            int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-            decorView.setSystemUiVisibility(option);
-            activity.getWindow().setNavigationBarColor(Color.TRANSPARENT);
-            activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
-        } else {
-            setStatusBarColor(activity, cantHandleColor);
-        }
-    }
 
     /**
      * 隐藏状态栏和虚拟按键
@@ -64,41 +50,49 @@ public class LshSystemUtils {
     }
 
     /**
-     * 设置状态栏颜色 * * @param activity 需要设置的activity * @param color 状态栏颜色值
+     * 设置沉浸式状态栏, 为状态栏着色
      */
-    public static void setStatusBarColor(Activity activity, int color) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            // 设置状态栏透明
-            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            // 生成一个状态栏大小的矩形
-            View statusView = createStatusView(activity, color);
-            // 添加 statusView 到布局中
-            ViewGroup decorView = (ViewGroup) activity.getWindow().getDecorView();
-            decorView.addView(statusView);
-            // 设置根布局的参数
-            ViewGroup rootView = (ViewGroup) ((ViewGroup) activity.findViewById(android.R.id.content)).getChildAt(0);
-            rootView.setFitsSystemWindows(true);
-            rootView.setClipToPadding(true);
-        } else {
-            LshLogUtils.w("SDK(>=19)版本过低,不支持: API=" + Build.VERSION.SDK_INT);
-        }
+    public static void setTranslucentStatusBar(Activity activity, int statusBarColor) {
+        setTranslucentStatusBar(activity, statusBarColor, false);
     }
 
-    /**
-     * 生成一个和状态栏大小相同的矩形条 * * @param activity 需要设置的activity * @param color 状态栏颜色值 * @return 状态栏矩形条
-     */
-    private static View createStatusView(Activity activity, int color) {
-        // 获得状态栏高度
-        int resourceId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
-        int statusBarHeight = activity.getResources().getDimensionPixelSize(resourceId);
+    public static void setTranslucentStatusBarWithInsertion(Activity activity, int statusBarColor) {
+        setTranslucentStatusBar(activity, statusBarColor, true);
+    }
 
-        // 绘制一个和状态栏一样高的矩形
-        View statusView = new View(activity);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                statusBarHeight);
-        statusView.setLayoutParams(params);
-        statusView.setBackgroundColor(color);
-        return statusView;
+    private static void setTranslucentStatusBar(Activity activity, int statusBarColor, boolean insert) {
+        if (Build.VERSION.SDK_INT < 19) {
+            return;
+        }
+        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//        if (Build.VERSION.SDK_INT >= 21) {
+//            // 获取主题中 colorPrimaryDark 的颜色
+//            TypedValue typedValue = new TypedValue();
+//            TypedArray typedArray = activity.obtainStyledAttributes(typedValue.data, new int[]{R.attr.colorPrimaryDark});
+//            int color = typedArray.getColor(0, 0);
+//            typedArray.recycle();
+//            // 没有指定 colorPrimaryDark 时, 默认是黑色
+//            if (color != Color.BLACK) {
+//                return;
+//            }
+//        }
+        ViewGroup contentView = (ViewGroup) activity.getWindow().getDecorView();
+        int statusBarHeight = LshScreenUtils.getStatusBarHeight();
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, statusBarHeight);
+        if (!insert) {
+            View statusBarBg = new View(activity);
+            statusBarBg.setBackgroundColor(statusBarColor);
+            contentView.addView(statusBarBg, 0, params);
+        } else {
+            View statusBarBg = new View(activity);
+            View statusBarFg = new View(activity);
+            int translucentColor = Color.argb(0x33, 0, 0, 0);
+            int toolbarColor = LshColorUtils.extractColor(statusBarColor, translucentColor);
+            statusBarBg.setBackgroundColor(toolbarColor);
+            statusBarFg.setBackgroundColor(translucentColor);
+            contentView.addView(statusBarBg, 0, params);
+            contentView.addView(statusBarFg, params);
+        }
     }
 
 }
