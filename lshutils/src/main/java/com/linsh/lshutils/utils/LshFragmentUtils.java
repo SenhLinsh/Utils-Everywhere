11ed64b52c2d93321ca03e4357ac7d4b67172dbc
SenhLinsh <lyuanxiang0812@126.com>
2017-06-30 17:36:51 +0800

    更新 LshFragmentUtils 支持v4包的Fragment

--- lshutils/src/main/java/com/linsh/lshutils/utils/LshFragmentUtils.java
+++ lshutils/src/main/java/com/linsh/lshutils/utils/LshFragmentUtils.java
@@ -1,21 +1,31 @@
 package com.linsh.lshutils.utils;
 
+
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+;
 
 /**
  * Created by Senh Linsh on 16/12/2.
  */
 public class LshFragmentUtils {
 
-    public static void replaceFragment(Fragment fragment, int containerViewId, Activity activity) {
+    public static void replaceFragment(android.app.Fragment fragment, int containerViewId, Activity activity) {
         if (fragment != null) {
-            FragmentManager fm = activity.getFragmentManager();
-            FragmentTransaction ft = fm.beginTransaction();
+            android.app.FragmentTransaction ft = activity.getFragmentManager().beginTransaction();
             ft.replace(containerViewId, fragment);
             ft.commit();
+        }
+    }
+
+    public static void replaceFragment(Fragment fragment, int containerViewId, AppCompatActivity activity) {
+        if (fragment != null) {
+            FragmentTransaction transaction = activity.getSupportFragmentManager().beginTransaction();
+            transaction.replace(containerViewId, fragment);
+            transaction.commit();
         }
     }
 }
