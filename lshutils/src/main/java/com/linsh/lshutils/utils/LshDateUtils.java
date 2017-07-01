b5cdccf59fa4c3d987b5c98294e237e574ec8543
SenhLinsh <lyuanxiang0812@126.com>
2017-07-02 02:18:21 +0800

    更新 LshDateUtils 添加获取农历日期的方法

--- lshutils/src/main/java/com/linsh/lshutils/utils/LshDateUtils.java
+++ lshutils/src/main/java/com/linsh/lshutils/utils/LshDateUtils.java
@@ -1,5 +1,6 @@
 package com.linsh.lshutils.utils;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -40,5 +41,38 @@ public class LshDateUtils {
                 break;
         }
         return prefix;
+    }
+
+    public static String getLunarDate(Date date) {
+        return getLunarDate(date, true);
+    }
+
+    public static String getLunarDate(Date date, boolean hasYear) {
+        StringBuilder builder = new StringBuilder();
+
+        String[] chineseMonths = {"正", "二", "三", "四", "五", "六", "七", "八", "九", "十", "冬", "腊"};
+        String[] chineseDayPres = {"初", "十", "廿", "三十"};
+        String[] chineseDays = {"一", "二", "三", "四", "五", "六", "七", "八", "九", "十"};
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        if (hasYear) {
+            int year = calendar.get(Calendar.YEAR);
+            while (year > 0) {
+                builder.insert(0, chineseDays[year % 10 - 1]);
+                year /= 10;
+            }
+            builder.append("年");
+        }
+        int month = calendar.get(Calendar.MONTH);
+        builder.append(chineseMonths[month]).append("月");
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        if (day == 10) {
+            builder.append("初十");
+        } else if (day == 20) {
+            builder.append("二十");
+        } else {
+            builder.append(chineseDayPres[day / 10]).append(chineseDays[day % 10]);
+        }
+        return builder.toString();
     }
 }
