83bcff8b7db0259e2243edfbcd28222cede12399
SenhLinsh <lyuanxiang0812@126.com>
2017-07-06 15:16:09 +0800

    新增 LshCameraUtils

---
+++ lshutils/src/main/java/com/linsh/lshutils/utils/LshCameraUtils.java
@@ -0,0 +1,41 @@
+package com.linsh.lshutils.utils;
+
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+
+import com.linsh.lshutils.utils.Basic.LshApplicationUtils;
+
+/**
+ * Created by Senh Linsh on 17/7/6.
+ */
+
+public class LshCameraUtils {
+
+    public static boolean checkCameraHardware() {
+        if (LshApplicationUtils.getContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean hasBackFacingCamera() {
+        return checkCameraFacing(Camera.CameraInfo.CAMERA_FACING_BACK);
+    }
+
+    public static boolean hasFrontFacingCamera() {
+        return checkCameraFacing(Camera.CameraInfo.CAMERA_FACING_FRONT);
+    }
+
+    private static boolean checkCameraFacing(final int facing) {
+        final int cameraCount = Camera.getNumberOfCameras();
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        for (int i = 0; i < cameraCount; i++) {
+            Camera.getCameraInfo(i, info);
+            if (facing == info.facing) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
