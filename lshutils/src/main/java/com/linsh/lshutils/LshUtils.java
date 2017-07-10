0d5e55ff0ff33fa470e921f11f3dc71caece46d3
SenhLinsh <lyuanxiang0812@126.com>
2017-07-10 17:17:28 +0800

    更新 LshUtils

--- lshutils/src/main/java/com/linsh/lshutils/LshUtils.java
+++ lshutils/src/main/java/com/linsh/lshutils/LshUtils.java
@@ -1,8 +1,12 @@
 package com.linsh.lshutils;
 
+import android.app.Application;
 import android.content.Context;
 
 import com.linsh.lshutils.utils.Basic.LshApplicationUtils;
+import com.linsh.lshutils.utils.Basic.LshLogUtils;
+import com.linsh.lshutils.utils.LshActivityLifecycleUtils;
+import com.linsh.lshutils.utils.LshBackgroundUtils;
 
 /**
  * Created by Senh Linsh on 17/7/6.
@@ -12,5 +16,25 @@ public class LshUtils {
 
     public static void init(Context context) {
         LshApplicationUtils.init(context);
+    }
+
+    public static void initLogUtils(boolean isDebug) {
+        LshLogUtils.init(isDebug);
+    }
+
+    public static void initLogUtilsTracer(int mainCount, int maxCount) {
+        LshLogUtils.Tracer.init(mainCount, maxCount);
+    }
+
+    public static void initLogUtilsPrinter(String logFilePath) {
+        LshLogUtils.Printer.setLogFilePath(logFilePath);
+    }
+
+    public static void initActivityLifecycleUtils(Application application) {
+        LshActivityLifecycleUtils.init(application);
+    }
+
+    public static void initBackgroundUtils(Application application) {
+        LshBackgroundUtils.init(application);
     }
 }
