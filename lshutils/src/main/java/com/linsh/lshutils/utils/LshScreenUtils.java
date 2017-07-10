473842adb4686859b518099fafb28dd1063fe2e4
SenhLinsh <lyuanxiang0812@126.com>
2017-07-10 16:55:51 +0800

    更新 LshScreenUtils

--- lshutils/src/main/java/com/linsh/lshutils/utils/LshScreenUtils.java
+++ lshutils/src/main/java/com/linsh/lshutils/utils/LshScreenUtils.java
@@ -1,16 +1,18 @@
 package com.linsh.lshutils.utils;
 
+import android.app.Activity;
+import android.app.KeyguardManager;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
 import android.util.DisplayMetrics;
 import android.view.Surface;
 import android.view.View;
 import android.view.WindowManager;
 
 import com.linsh.lshutils.utils.Basic.LshApplicationUtils;
-
-import static com.linsh.lshutils.utils.LshScreenUtils.Orientation.LANDSCAPE;
-import static com.linsh.lshutils.utils.LshScreenUtils.Orientation.PORTRAIT;
 
 /**
  * Created by Senh Linsh on 17/3/30.
@@ -49,17 +51,31 @@ public class LshScreenUtils {
         return Math.max(outMetrics.widthPixels, outMetrics.heightPixels);
     }
 
-    public static int getScreenRotation() {
-        WindowManager wm = (WindowManager) LshApplicationUtils.getContext().getSystemService(Context.WINDOW_SERVICE);
-        return wm.getDefaultDisplay().getRotation();
+    public static boolean isLandscape() {
+        return LshContextUtils.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
     }
 
-    public static Orientation getScreenOrientation() {
-        int rotation = getScreenRotation();
-        if (rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_180) {
-            return PORTRAIT;
-        } else {
-            return LANDSCAPE;
+    public static boolean isPortrait() {
+        return LshContextUtils.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+    }
+
+    /**
+     * 获取屏幕旋转角度
+     * <p>0 为正常竖屏, 180 为倒置竖屏, 90 270 为横屏</p>
+     */
+    public static int getScreenRotation(final Activity activity) {
+        WindowManager wm = (WindowManager) LshApplicationUtils.getContext().getSystemService(Context.WINDOW_SERVICE);
+        int rotation = wm.getDefaultDisplay().getRotation();
+        switch (rotation) {
+            default:
+            case Surface.ROTATION_0:
+                return 0;
+            case Surface.ROTATION_90:
+                return 90;
+            case Surface.ROTATION_180:
+                return 180;
+            case Surface.ROTATION_270:
+                return 270;
         }
     }
 
@@ -83,7 +99,54 @@ public class LshScreenUtils {
         return resources.getDimensionPixelSize(resourceId);
     }
 
-    public enum Orientation {
-        LANDSCAPE, PORTRAIT
+    /**
+     * 获取当前屏幕截图，包含状态栏
+     */
+    public static Bitmap getScreenShot(Activity activity) {
+        View view = activity.getWindow().getDecorView();
+        view.setDrawingCacheEnabled(true);
+        view.buildDrawingCache();
+        Bitmap bmp = view.getDrawingCache();
+        int width = getScreenWidth();
+        int height = getScreenHeight();
+        Bitmap bp = null;
+        bp = Bitmap.createBitmap(bmp, 0, 0, width, height);
+        view.destroyDrawingCache();
+        return bp;
+    }
+
+    /**
+     * 获取当前屏幕截图，不包含状态栏
+     *
+     * @param activity
+     * @return
+     */
+    public static Bitmap getScreenShotWithoutStatusBar(Activity activity) {
+        View view = activity.getWindow().getDecorView();
+        view.setDrawingCacheEnabled(true);
+        view.buildDrawingCache();
+        Bitmap bmp = view.getDrawingCache();
+
+        Rect frame = new Rect();
+        activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+        int statusBarHeight = frame.top;
+
+        int width = getScreenWidth();
+        int height = getScreenHeight();
+
+        Bitmap bp = null;
+        bp = Bitmap.createBitmap(bmp, 0, statusBarHeight, width, height
+                - statusBarHeight);
+        view.destroyDrawingCache();
+        return bp;
+    }
+
+
+    /**
+     * 判断是否锁屏
+     */
+    public static boolean isScreenLock() {
+        KeyguardManager km = (KeyguardManager) LshContextUtils.getSystemService(Context.KEYGUARD_SERVICE);
+        return km.inKeyguardRestrictedInputMode();
     }
 }
