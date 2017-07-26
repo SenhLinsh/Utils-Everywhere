97fd4731b0fe83579217b3379d6253a7593bfe9d
SenhLinsh <lyuanxiang0812@126.com>
2017-07-26 21:25:14 +0800

    新增 LshOSUtils

---
+++ lshutils/src/main/java/com/linsh/lshutils/utils/LshOSUtils.java
@@ -0,0 +1,17 @@
+package com.linsh.lshutils.utils;
+
+/**
+ * Created by Senh Linsh on 17/7/25.
+ */
+
+public class LshOSUtils {
+
+    public enum ROM {
+        MIUI, // 小米
+        Flyme, // 魅族
+        FuntouchOS, // vivo
+        ColorOS, // OPPO
+        EUI, // 乐视
+        EMUI, // 华为
+    }
+}
