cf33e2c8bcf128dceea614f9dd0332d4e97a3f9c
SenhLinsh <lyuanxiang0812@126.com>
2017-08-03 18:41:28 +0800

    更新 LshOSUtils

--- lshutils/src/main/java/com/linsh/lshutils/utils/LshOSUtils.java
+++ lshutils/src/main/java/com/linsh/lshutils/utils/LshOSUtils.java
@@ -9,9 +9,27 @@ public class LshOSUtils {
     public enum ROM {
         MIUI, // 小米
         Flyme, // 魅族
-        FuntouchOS, // vivo
-        ColorOS, // OPPO
-        EUI, // 乐视
         EMUI, // 华为
+        ColorOS, // OPPO
+        FuntouchOS, // vivo
+        SmartisanOS, // 锤子
+        EUI, // 乐视
+        LGE, // LG
+        Sense, // HTC
+        AmigoOS, // 金立
+        _360OS, // 奇酷360
+        NubiaUI, // 努比亚
+        H2OS, // 一加
+        YunOS, // 阿里巴巴
+        YuLong, // 酷派
+
+        SamSung, // 三星
+        Sony, // 索尼
+        Lenovo, // 联想
+        LG, // LG
+
+        Google, // 原生
+
+        Other; // CyanogenMod, Lewa OS, 百度云OS, Tencent OS, 深度OS, IUNI OS, Tapas OS, Mokee
     }
 }
