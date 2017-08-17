ddf93cd3a73466c3aa246bc81856142cb1bf198b
SenhLinsh <lyuanxiang0812@126.com>
2017-08-17 18:15:30 +0800

    新增 LshFileManagerUtils 管理文件/文件夹

--- lshutils/src/main/java/com/linsh/lshutils/LshUtils.java
+++ lshutils/src/main/java/com/linsh/lshutils/LshUtils.java
@@ -7,6 +7,9 @@ import com.linsh.lshutils.utils.Basic.LshApplicationUtils;
 import com.linsh.lshutils.utils.Basic.LshLogUtils;
 import com.linsh.lshutils.utils.LshActivityLifecycleUtils;
 import com.linsh.lshutils.utils.LshBackgroundUtils;
+import com.linsh.lshutils.utils.LshFileManagerUtils;
+
+import java.io.File;
 
 /**
  * Created by Senh Linsh on 17/7/6.
@@ -28,6 +31,10 @@ public class LshUtils {
 
     public static void initLogUtilsPrinter(String logFilePath) {
         LshLogUtils.Printer.setLogFilePath(logFilePath);
+    }
+
+    public static void initFileManagerUtils(File appDir) {
+        LshFileManagerUtils.initAppDir(appDir);
     }
 
     public static void initActivityLifecycleUtils(Application application) {
---
+++ lshutils/src/main/java/com/linsh/lshutils/utils/LshFileManagerUtils.java
@@ -0,0 +1,56 @@
+package com.linsh.lshutils.utils;
+
+import android.os.Environment;
+
+import com.linsh.lshutils.utils.Basic.LshFileUtils;
+
+import java.io.File;
+
+/**
+ * Created by Senh Linsh on 17/8/15.
+ */
+
+public class LshFileManagerUtils {
+
+    private static File sAppDir = new File(Environment.getExternalStorageDirectory(), LshAppUtils.getPackageName());
+
+    public static void initAppDir(File file) {
+        sAppDir = file;
+    }
+
+    public static File getAppDir() {
+        if (LshFileUtils.checkPermission()) {
+            if (!sAppDir.exists()) {
+                sAppDir.mkdirs();
+            }
+            return sAppDir;
+        }
+        return LshContextUtils.getFilesDir();
+    }
+
+    public static File getDataDir() {
+        if (LshFileUtils.checkPermission()) {
+            return LshContextUtils.get().getExternalFilesDir(null);
+        }
+        return LshContextUtils.get().getFilesDir();
+    }
+
+    public static File getFile(String fileName) {
+        return new File(getAppDir(), fileName);
+    }
+
+    public static File getFile(File parentDir, String fileName) {
+        parentDir.mkdirs();
+        return new File(parentDir, fileName);
+    }
+
+    public static File getDir(String dirName) {
+        return getDir(getAppDir(), dirName);
+    }
+
+    public static File getDir(File parentDir, String dirName) {
+        File dir = new File(parentDir, dirName);
+        dir.mkdirs();
+        return dir;
+    }
+}
