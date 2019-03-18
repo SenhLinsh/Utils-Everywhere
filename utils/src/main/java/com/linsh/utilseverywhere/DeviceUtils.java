df42c77976b510fc75d92ebf1b619f188dd550b8
Senh Linsh <lyuanxiang0812@126.com>
2019-03-18 14:02:58 +0800

    feat: 标记 PhoneUtils 为过时, 迁移至 DeviceUtils 中, 并新增部分方法

--- utils/src/main/java/com/linsh/utilseverywhere/DeviceUtils.java
+++ utils/src/main/java/com/linsh/utilseverywhere/DeviceUtils.java
@@ -1,7 +1,16 @@
 package com.linsh.utilseverywhere;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.os.Build;
+import android.support.annotation.RequiresPermission;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <pre>
@@ -24,9 +33,133 @@ public class DeviceUtils {
      * 判断是否为手机设备
      *
      * @return true 为手机, false 为平板
+     * @deprecated 该方法已过时, 请使用 {@link DeviceUtils#getScreenSizeType()} 自行判断屏幕大小
      */
+    @Deprecated
     public static boolean isPhoneDevice() {
         return (getContext().getResources().getConfiguration().screenLayout
                 & Configuration.SCREENLAYOUT_SIZE_MASK) < Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+
+    /**
+     * 判断是否为通讯设备, 即是否可以打电话
+     *
+     * @return 是否为通讯设备
+     */
+    public static boolean isTelephonyDevice() {
+        TelephonyManager tm = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
+        return tm != null && tm.getPhoneType() != TelephonyManager.PHONE_TYPE_NONE;
+    }
+
+    /**
+     * 获取屏幕尺寸类型
+     * <p>
+     * {@link Configuration#SCREENLAYOUT_SIZE_SMALL} : 至少约为 320x426 dp
+     * {@link Configuration#SCREENLAYOUT_SIZE_NORMAL} : 至少约为 320x470 dp
+     * {@link Configuration#SCREENLAYOUT_SIZE_LARGE} : 至少约为 480x640 dp
+     * {@link Configuration#SCREENLAYOUT_SIZE_XLARGE} : 至少约为 720x960 dp
+     *
+     * @return 屏幕尺寸类型
+     */
+    public static int getScreenSizeType() {
+        return getContext().getResources().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK;
+    }
+
+    /**
+     * 获取设备厂商, 如: Xiaomi
+     *
+     * @return 设备厂商
+     */
+    public static String getManufacturer() {
+        return Build.MANUFACTURER;
+    }
+
+    /**
+     * 获取设备品牌
+     *
+     * @return 设备品牌
+     */
+    public static String getMobileBrand() {
+        return Build.BRAND;
+    }
+
+    /**
+     * 获取设备型号
+     *
+     * @return 设备型号
+     */
+    public static String getMobileModel() {
+        return Build.MODEL;
+    }
+
+    /**
+     * 获取手机 IMEI 码
+     * <p>需添加权限 {@code <uses-permission android:name="android.permission.READ_PHONE_STATE"/>}</p>
+     *
+     * @return IMEI 码
+     */
+    @RequiresPermission(Manifest.permission.READ_PHONE_STATE)
+    public static String getIMEI() {
+        TelephonyManager tm = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
+        if (tm == null) return null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String res = tm.getImei();
+            if (!TextUtils.isEmpty(res))
+                return res;
+            res = tm.getMeid();
+            if (!TextUtils.isEmpty(res))
+                return res;
+            return tm.getDeviceId();
+        } else {
+            return tm.getDeviceId();
+        }
+    }
+
+    /**
+     * 获取 IMSI 码
+     * <p>需添加权限 {@code <uses-permission android:name="android.permission.READ_PHONE_STATE"/>}</p>
+     *
+     * @return IMSI 码
+     */
+    @RequiresPermission(android.Manifest.permission.READ_PHONE_STATE)
+    public static String getIMSI() {
+        TelephonyManager tm = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
+        return tm != null ? tm.getSubscriberId() : null;
+    }
+
+    /**
+     * 获取序列号, 如果存在的话
+     */
+    @RequiresPermission(Manifest.permission.READ_PHONE_STATE)
+    public static String getSerial() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return Build.getSerial();
+        } else {
+            return Build.SERIAL;
+        }
+    }
+
+    /**
+     * 获取设备硬件信息
+     *
+     * @return 硬件信息
+     */
+    public static List<String> getDeviceInfo() {
+        ArrayList<String> list = new ArrayList<>();
+        // 通过反射获取系统的硬件信息
+        try {
+            Field[] fields = Build.class.getDeclaredFields();
+            for (Field field : fields) {
+                //暴力反射 ,获取私有的信息
+                field.setAccessible(true);
+                String name = field.getName();
+                String value = field.get(null).toString();
+                list.add(name + "=" + value);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 }
--- utils/src/main/java/com/linsh/utilseverywhere/PhoneUtils.java
+++ utils/src/main/java/com/linsh/utilseverywhere/PhoneUtils.java
@@ -3,10 +3,7 @@ package com.linsh.utilseverywhere;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Build;
-import android.telephony.TelephonyManager;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,8 +12,11 @@ import java.util.List;
  *    github : https://github.com/SenhLinsh
  *    date   : 2017/11/10
  *    desc   : 工具类: 手机相关
+ *
+ *            该类已过时, 相关方法将转移到 {@link DeviceUtils}
  * </pre>
  */
+@Deprecated
 public class PhoneUtils {
 
     private PhoneUtils() {
@@ -27,70 +27,54 @@ public class PhoneUtils {
     }
 
     /**
-     * 判断当前设备是否为手机
-     *
-     * @return 是否为手机
+     * see {@link DeviceUtils#isTelephonyDevice()}
      */
     public static boolean isPhone() {
-        TelephonyManager tm = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
-        return tm != null && tm.getPhoneType() != TelephonyManager.PHONE_TYPE_NONE;
+        return DeviceUtils.isTelephonyDevice();
     }
 
     /**
-     * 获取手机 IMEI 码
-     * <p>需添加权限 {@code <uses-permission android:name="android.permission.READ_PHONE_STATE"/>}</p>
-     *
-     * @return IMEI 码
+     * see {@link DeviceUtils#getIMEI()}
      */
-    @SuppressLint({"HardwareIds", "MissingPermission"})
+    @SuppressLint("MissingPermission")
     public static String getIMEI() {
-        TelephonyManager tm = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
-        return tm != null ? tm.getDeviceId() : null;
+        return DeviceUtils.getIMEI();
     }
 
     /**
-     * 获取 IMSI 码
-     * <p>需添加权限 {@code <uses-permission android:name="android.permission.READ_PHONE_STATE"/>}</p>
-     *
-     * @return IMSI 码
+     * see {@link DeviceUtils#getIMSI()}
      */
-    @SuppressLint({"HardwareIds", "MissingPermission"})
+    @SuppressLint("MissingPermission")
     public static String getIMSI() {
-        TelephonyManager tm = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
-        return tm != null ? tm.getSubscriberId() : null;
+        return DeviceUtils.getIMSI();
     }
 
     /**
-     * 获取设备厂商, 如: Xiaomi
-     *
-     * @return 设备厂商
+     * see {@link DeviceUtils#getManufacturer()}
      */
     public static String getManufacturer() {
-        return Build.MANUFACTURER;
+        return DeviceUtils.getManufacturer();
     }
 
     /**
-     * 获取设备品牌
-     *
-     * @return 设备品牌
+     * see {@link DeviceUtils#getMobileBrand()}
      */
     public static String getMobileBrand() {
-        return Build.BRAND;
+        return DeviceUtils.getMobileBrand();
     }
 
     /**
-     * 获取设备型号
-     *
-     * @return 设备型号
+     * see {@link DeviceUtils#getMobileModel()}
      */
     public static String getMobileModel() {
-        String model = Build.MODEL;
-        if (model != null) {
-            model = model.trim();
-        } else {
-            model = "";
-        }
-        return model;
+        return DeviceUtils.getMobileModel();
+    }
+
+    /**
+     * see {@link DeviceUtils#getDeviceInfo()}
+     */
+    public static List<String> getMobileInfo() {
+        return DeviceUtils.getDeviceInfo();
     }
 
     /**
@@ -109,28 +93,5 @@ public class PhoneUtils {
      */
     public static String getSDKVersionName() {
         return Build.VERSION.RELEASE;
-    }
-
-    /**
-     * 获取设备硬件信息
-     *
-     * @return 硬件信息
-     */
-    public static List<String> getMobileInfo() {
-        ArrayList<String> list = new ArrayList<>();
-        // 通过反射获取系统的硬件信息
-        try {
-            Field[] fields = Build.class.getDeclaredFields();
-            for (Field field : fields) {
-                //暴力反射 ,获取私有的信息
-                field.setAccessible(true);
-                String name = field.getName();
-                String value = field.get(null).toString();
-                list.add(name + "=" + value);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return list;
     }
 }
