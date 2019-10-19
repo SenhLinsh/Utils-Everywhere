496f886b96795e39876060713924e1db6c1cee1a
lin.shenghe <lin.shenghe@imprexion.cn>
2019-10-19 19:41:50 +0800

    ✔️ Update UriUtils

--- ue/src/main/java/com/linsh/utilseverywhere/UriUtils.java
+++ ue/src/main/java/com/linsh/utilseverywhere/UriUtils.java
@@ -1,72 +1,220 @@
 package com.linsh.utilseverywhere;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.os.storage.StorageManager;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 
 import java.io.File;
+import java.lang.reflect.Array;
+import java.lang.reflect.Method;
 
 /**
  * <pre>
- *    author : Senh Linsh
- *    github : https://github.com/SenhLinsh
- *    date   : 2017/11/14
- *    desc   : 工具类: Uri 相关
+ *     author: Blankj
+ *     blog  : http://blankj.com
+ *     time  : 2018/04/20
+ *     desc  : URI 相关
+ *
+ *              注: 该类直接参考使用 https://github.com/Blankj/AndroidUtilCode 中 UriUtils
  * </pre>
  */
-public class UriUtils {
+public final class UriUtils {
 
     private UriUtils() {
-    }
-
-    private static Context getContext() {
-        return ContextUtils.get();
+        throw new UnsupportedOperationException("u can't instantiate me...");
     }
 
     /**
-     * 从 Uri 中获取文件路径
+     * File to uri.
      *
-     * @param uri Uri
-     * @return 文件路径
+     * @param file The file.
+     * @return uri
      */
-    public static String getFilePathFromUri(Uri uri) {
-        if ("content".equals(uri.getScheme())) {
-            Cursor cursor = null;
-            final String column = "_data";
-            final String[] projection = {column};
-            try {
-                cursor = getContext().getContentResolver().query(uri, projection, null, null, null);
-                if (cursor != null && cursor.moveToFirst()) {
-                    final int column_index = cursor.getColumnIndexOrThrow(column);
-                    return cursor.getString(column_index);
-                }
-            } finally {
-                if (cursor != null)
-                    cursor.close();
-            }
-        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
-            return uri.getPath();
+    public static Uri file2Uri(@NonNull final File file) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            String authority = ContextUtils.getPackageName() + ".utilcode.provider";
+            return FileProvider.getUriForFile(ContextUtils.get(), authority, file);
+        } else {
+            return Uri.fromFile(file);
         }
-        return null;
     }
 
     /**
-     * 获取指定文件的 Uri
+     * Uri to file.
      *
-     * @param file 指定文件
-     * @return Uri
+     * @param uri The uri.
+     * @return file
      */
-    public static Uri getUriFromFile(File file) {
-        return Uri.fromFile(file);
+    public static File uri2File(@NonNull final Uri uri) {
+        Log.d("UriUtils", uri.toString());
+        String authority = uri.getAuthority();
+        String scheme = uri.getScheme();
+        String path = uri.getPath();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+                && path != null && path.startsWith("/external/")) {
+            File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath()
+                    + path.replace("/external", ""));
+            if (file.exists()) {
+                Log.d("UriUtils", uri.toString() + " -> /external");
+                return file;
+            }
+        }
+        if (ContentResolver.SCHEME_FILE.equals(scheme)) {
+            if (path != null) return new File(path);
+            Log.d("UriUtils", uri.toString() + " parse failed. -> 0");
+            return null;
+        }// end 0
+        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
+                && DocumentsContract.isDocumentUri(ContextUtils.get(), uri)) {
+            if ("com.android.externalstorage.documents".equals(authority)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                if ("primary".equalsIgnoreCase(type)) {
+                    return new File(Environment.getExternalStorageDirectory() + "/" + split[1]);
+                } else {
+                    // Below logic is how External Storage provider build URI for documents
+                    // http://stackoverflow.com/questions/28605278/android-5-sd-card-label
+                    StorageManager mStorageManager = (StorageManager) ContextUtils.getSystemService(Context.STORAGE_SERVICE);
+                    try {
+                        Class<?> storageVolumeClazz = Class.forName("android.os.storage.StorageVolume");
+                        Method getVolumeList = mStorageManager.getClass().getMethod("getVolumeList");
+                        Method getUuid = storageVolumeClazz.getMethod("getUuid");
+                        Method getState = storageVolumeClazz.getMethod("getState");
+                        Method getPath = storageVolumeClazz.getMethod("getPath");
+                        Method isPrimary = storageVolumeClazz.getMethod("isPrimary");
+                        Method isEmulated = storageVolumeClazz.getMethod("isEmulated");
+
+                        Object result = getVolumeList.invoke(mStorageManager);
+
+                        final int length = Array.getLength(result);
+                        for (int i = 0; i < length; i++) {
+                            Object storageVolumeElement = Array.get(result, i);
+                            //String uuid = (String) getUuid.invoke(storageVolumeElement);
+
+                            final boolean mounted = Environment.MEDIA_MOUNTED.equals(getState.invoke(storageVolumeElement))
+                                    || Environment.MEDIA_MOUNTED_READ_ONLY.equals(getState.invoke(storageVolumeElement));
+
+                            //if the media is not mounted, we need not get the volume details
+                            if (!mounted) continue;
+
+                            //Primary storage is already handled.
+                            if ((Boolean) isPrimary.invoke(storageVolumeElement)
+                                    && (Boolean) isEmulated.invoke(storageVolumeElement)) {
+                                continue;
+                            }
+
+                            String uuid = (String) getUuid.invoke(storageVolumeElement);
+
+                            if (uuid != null && uuid.equals(type)) {
+                                return new File(getPath.invoke(storageVolumeElement) + "/" + split[1]);
+                            }
+                        }
+                    } catch (Exception ex) {
+                        Log.d("UriUtils", uri.toString() + " parse failed. " + ex.toString() + " -> 1_0");
+                    }
+                }
+                Log.d("UriUtils", uri.toString() + " parse failed. -> 1_0");
+                return null;
+            }// end 1_0
+            else if ("com.android.providers.downloads.documents".equals(authority)) {
+                final String id = DocumentsContract.getDocumentId(uri);
+                if (!TextUtils.isEmpty(id)) {
+                    try {
+                        final Uri contentUri = ContentUris.withAppendedId(
+                                Uri.parse("content://downloads/public_downloads"),
+                                Long.valueOf(id)
+                        );
+                        return getFileFromUri(contentUri, "1_1");
+                    } catch (NumberFormatException e) {
+                        if (id.startsWith("raw:")) {
+                            return new File(id.substring(4));
+                        }
+                    }
+                }
+                Log.d("UriUtils", uri.toString() + " parse failed. -> 1_1");
+                return null;
+            }// end 1_1
+            else if ("com.android.providers.media.documents".equals(authority)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                Uri contentUri;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                } else {
+                    Log.d("UriUtils", uri.toString() + " parse failed. -> 1_2");
+                    return null;
+                }
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{split[1]};
+                return getFileFromUri(contentUri, selection, selectionArgs, "1_2");
+            }// end 1_2
+            else if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
+                return getFileFromUri(uri, "1_3");
+            }// end 1_3
+            else {
+                Log.d("UriUtils", uri.toString() + " parse failed. -> 1_4");
+                return null;
+            }// end 1_4
+        }// end 1
+        else if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
+            return getFileFromUri(uri, "2");
+        }// end 2
+        else {
+            Log.d("UriUtils", uri.toString() + " parse failed. -> 3");
+            return null;
+        }// end 3
     }
 
-    /**
-     * Android 7.0 获取指定文件的 Provider Uri
-     *
-     * @param file 指定文件
-     * @return Uri
-     */
-    public static Uri getProviderUriFromFile(File file) {
-        return FileProviderUtils.getUriForFile(file);
+    private static File getFileFromUri(final Uri uri, final String code) {
+        return getFileFromUri(uri, null, null, code);
+    }
+
+    private static File getFileFromUri(final Uri uri,
+                                       final String selection,
+                                       final String[] selectionArgs,
+                                       final String code) {
+        final Cursor cursor = ContextUtils.getContentResolver().query(
+                uri, new String[]{"_data"}, selection, selectionArgs, null);
+        if (cursor == null) {
+            Log.d("UriUtils", uri.toString() + " parse failed(cursor is null). -> " + code);
+            return null;
+        }
+        try {
+            if (cursor.moveToFirst()) {
+                final int columnIndex = cursor.getColumnIndex("_data");
+                if (columnIndex > -1) {
+                    return new File(cursor.getString(columnIndex));
+                } else {
+                    Log.d("UriUtils", uri.toString() + " parse failed(columnIndex: " + columnIndex + " is wrong). -> " + code);
+                    return null;
+                }
+            } else {
+                Log.d("UriUtils", uri.toString() + " parse failed(moveToFirst return false). -> " + code);
+                return null;
+            }
+        } catch (Exception e) {
+            Log.d("UriUtils", uri.toString() + " parse failed. -> " + code);
+            return null;
+        } finally {
+            cursor.close();
+        }
     }
 }
