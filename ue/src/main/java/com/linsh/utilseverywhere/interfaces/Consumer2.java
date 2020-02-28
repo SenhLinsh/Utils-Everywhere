a84121745ea7d89748f6084e60386f404bb96616
Senh Linsh <lyuanxiang0812@126.com>
2020-02-29 04:20:17 +0800

    ➕ 添加接口

---
+++ ue/src/main/java/com/linsh/utilseverywhere/interfaces/Consumer2.java
@@ -0,0 +1,14 @@
+package com.linsh.utilseverywhere.interfaces;
+
+/**
+ * <pre>
+ *    author : Senh Linsh
+ *    github : https://github.com/SenhLinsh
+ *    date   : 2020/02/23
+ *    desc   :
+ * </pre>
+ */
+public interface Consumer2<T1, T2> {
+
+    void accept(T1 t1, T2 t2);
+}
