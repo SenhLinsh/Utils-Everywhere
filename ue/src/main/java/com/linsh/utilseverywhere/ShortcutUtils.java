6c5c964281d7c81eafe477d240044eeb66734a1d
Sehn Linsh <lyuanxiang0812@126.com>
2020-06-03 16:22:34 +0800

    ✨ 优化 ShortcutUtils

--- ue/src/main/java/com/linsh/utilseverywhere/ShortcutUtils.java
+++ ue/src/main/java/com/linsh/utilseverywhere/ShortcutUtils.java
@@ -1,12 +1,21 @@
 package com.linsh.utilseverywhere;
 
 import android.app.Activity;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
+import android.content.IntentSender;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
+import android.graphics.drawable.Icon;
+import android.os.Build;
 import android.os.Parcelable;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.content.pm.ShortcutInfoCompat;
+import androidx.core.content.pm.ShortcutManagerCompat;
+import androidx.core.graphics.drawable.IconCompat;
+
+import java.util.Collections;
 
 /**
  * <pre>
@@ -18,7 +27,6 @@ import android.os.Parcelable;
  *          该工具类存在部分问题, 需要后期优化, 目前仅供参考
  * </pre>
  */
-@Deprecated
 public class ShortcutUtils {
 
     private ShortcutUtils() {
@@ -29,112 +37,112 @@ public class ShortcutUtils {
     }
 
     /**
-     * 创建桌面快捷方式, 点击跳转本应用
-     * <p>需要权限: <uses-permission android:name="com.android.launcher.permission.INSTALL_SHORTCUT" />
-     *
-     * @param name 名称
-     * @param icon 图标
-     */
-    public static void createShortcut(String name, int icon) {
-        if (hasShortcut(name)) {
-            return;
-        }
-        Context context = getContext();
-        // 指定动作名称
-        Intent shortcut = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
-        // 快捷方式名称
-        shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
-        // 不允许重复创建（不一定有效）
-        shortcut.putExtra("duplicate", false);
-        // 快捷方式的图标
-        Parcelable iconResource = Intent.ShortcutIconResource.fromContext(context, icon);
-        shortcut.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconResource);
-        shortcut.putExtra("createShortcut", "createShortcut");
-
-        Intent shortcutIntent = context.getPackageManager().getLaunchIntentForPackage(context.getPackageName());
-        shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-
-        context.sendBroadcast(shortcut);
-    }
-
-    /**
-     * 创建桌面快捷方式, 点击指定 Activity
-     * 权限: <uses-permission android:name="com.android.launcher.permission.INSTALL_SHORTCUT" />
+     * 创建桌面快捷方式
      *
      * @param name     名称
      * @param icon     图标
-     * @param activity 需要跳转的 Activity 的类
+     * @param activity 指定打开的页面
+     * @return 是否创建成功
      */
-    public static void createShortcut(String name, int icon, Class<? extends Activity> activity) {
-        // 先判断该快捷是否存在
-        if (hasShortcut(name)) {
-            return;
-        }
+    public static boolean createShortcut(String name, int icon, Class<? extends Activity> activity) {
+        return createShortcut(name, name, icon, activity);
+    }
+
+    /**
+     * 创建桌面快捷方式
+     *
+     * @param id       唯一标识
+     * @param name     名称
+     * @param icon     图标
+     * @param activity 点击跳转的 Activity
+     * @return 是否创建成功
+     */
+    public static boolean createShortcut(String id, String name, int icon, Class<? extends Activity> activity) {
         Context context = getContext();
-        Intent intent = new Intent();
-        // 指定动作名称
-        intent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
-        // 指定快捷方式的图标
+        if (ShortcutManagerCompat.isRequestPinShortcutSupported(context)) {
+            ShortcutInfoCompat info = new ShortcutInfoCompat.Builder(context, id)
+                    .setIcon(IconCompat.createWithResource(context, icon))
+                    .setShortLabel(name)
+                    .setIntent(new Intent(context, activity)
+                            .setAction(Intent.ACTION_VIEW))
+                    .build();
+            return ShortcutManagerCompat.requestPinShortcut(context, info, null);
+        }
+        return false;
+    }
+
+    /**
+     * Android O 及以下创建桌面快捷方式
+     *
+     * @param id       唯一标识
+     * @param name     名称
+     * @param icon     图标
+     * @param activity 点击跳转的 Activity
+     * @return 是否创建成功
+     * @deprecated 建议使用 {@link ShortcutManagerCompat#requestPinShortcut(Context, ShortcutInfoCompat, IntentSender)} 兼容
+     */
+    @Deprecated
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private static boolean createShortCutAboveAndroidO(String id, String name, int icon, Class<? extends Activity> activity) {
+        Context context = getContext();
+        ShortcutManager shortcutManager = (ShortcutManager) context.getSystemService(Context.SHORTCUT_SERVICE);
+        if (shortcutManager.isRequestPinShortcutSupported()) {
+            ShortcutInfo info = new ShortcutInfo.Builder(context, id)
+                    .setIcon(Icon.createWithResource(context, icon))
+                    .setShortLabel(name)
+                    .setIntent(new Intent(context, activity)
+                            .setAction(Intent.ACTION_VIEW))
+                    .build();
+            return shortcutManager.requestPinShortcut(info, null);
+        }
+        return false;
+    }
+
+    /**
+     * Android O 以下创建桌面快捷方式
+     *
+     * <p>需要权限: <uses-permission android:name="com.android.launcher.permission.INSTALL_SHORTCUT" />
+     *
+     * @param name      名称
+     * @param icon      图标
+     * @param activity  指定打开的页面
+     * @param duplicate 是否允许重复创建
+     * @deprecated 建议使用 {@link ShortcutManagerCompat#requestPinShortcut(Context, ShortcutInfoCompat, IntentSender)} 兼容
+     */
+    @Deprecated
+    private static void createShortcutBelowAndroidO(String name, int icon, Class<? extends Activity> activity, boolean duplicate) {
+        Context context = getContext();
+        // action: INSTALL_SHORTCUT
+        Intent shortcut = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
+        // 名称
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
+        // 图标
         Parcelable iconResource = Intent.ShortcutIconResource.fromContext(context, icon);
-        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconResource);
-        // 指定快捷方式的名称
-        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
-
-        // 指定快捷图标激活哪个activity
-        Intent i = new Intent();
-        i.setAction(Intent.ACTION_MAIN);
-        i.addCategory(Intent.CATEGORY_LAUNCHER);
-        ComponentName component = new ComponentName(context, activity);
-        i.setComponent(component);
-        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, i);
-
-        context.sendBroadcast(intent);
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconResource);
+        // 是否允许重复创建
+        shortcut.putExtra("duplicate", duplicate);
+        // 关联程序
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, new Intent(context, activity));
+        // 广播注册快捷方式
+        context.sendBroadcast(shortcut);
     }
 
     /**
      * 删除当前应用指点的桌面快捷方式
      *
-     * @param name 快捷方式名称
-     */
-    public static void delShortcut(String name) {
-        if (!hasShortcut(name)) {
-            return;
-        }
-        Context context = getContext();
-        Intent shortcut = new Intent("com.android.launcher.action.UNINSTALL_SHORTCUT");
-        shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
-        Intent shortcutIntent = context.getPackageManager().getLaunchIntentForPackage(context.getPackageName());
-        shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
-        context.sendBroadcast(shortcut);
-    }
-
-    /**
-     * 判断当前应用在桌面上是否存在指定的桌面快捷方式
-     * <p/>
-     * 注意:目前此方法暂时无效, 需要后期进行改进 // TODO: 17/11/13
-     *
-     * @param shortcutName 快捷方式名称
-     * @return 是否存在该快捷方式
+     * @param id 唯一标识
+     * @deprecated 未经测试验证
      */
     @Deprecated
-    public static boolean hasShortcut(String shortcutName) {
+    private static void removeShortcut(String id) {
         Context context = getContext();
-        boolean result = false;
-
-        final String uriStr;
-        if (android.os.Build.VERSION.SDK_INT < 8) {
-            uriStr = "content://com.android.launcher.settings/favorites?notify=true";
-        } else if (android.os.Build.VERSION.SDK_INT < 19) {
-            uriStr = "content://com.android.launcher2.settings/favorites?notify=true";
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N_MR1) {
+            ShortcutManager shortcutManager = (ShortcutManager) context.getSystemService(Context.SHORTCUT_SERVICE);
+            shortcutManager.disableShortcuts(Collections.singletonList(id));
         } else {
-            uriStr = "content://com.android.launcher3.settings/favorites?notify=true";
+            Intent shortcut = new Intent("com.android.launcher.action.UNINSTALL_SHORTCUT");
+            shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME, id);
+            context.sendBroadcast(shortcut);
         }
-        final Uri CONTENT_URI = Uri.parse(uriStr);
-        final Cursor c = context.getContentResolver().query(CONTENT_URI, null,
-                "title=?", new String[]{shortcutName}, null);
-        if (c != null && c.getCount() > 0) {
-            result = true;
-        }
-        return result;
     }
 }
